2e6703f6d6a4cc6f097a61a1d539a2c8484b86de
qbkira <qbuouo@gmail.com>
2014-06-04 09:02:53 +0800

    export_include base

--- cocos/Android.mk
+++ cocos/Android.mk
@@ -175,6 +175,7 @@ physics/chipmunk/CCPhysicsWorldInfo_chipmunk.cpp \
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH) \
                     $(LOCAL_PATH)/. \
+                    $(LOCAL_PATH)/base \
                     $(LOCAL_PATH)/platform/android \
                     $(LOCAL_PATH)/../external/tinyxml2 \
                     $(LOCAL_PATH)/../external/unzip \
