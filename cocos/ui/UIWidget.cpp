f01b0861fdaa28728614f671467602ac86bf6e77
dolly <woc2006@163.com>
2015-05-19 15:42:11 +0800

    在touch事件中, 支持remove

--- cocos/ui/UIWidget.cpp
+++ cocos/ui/UIWidget.cpp
@@ -719,6 +719,7 @@ void Widget::onTouchCancelled(Touch *touch, Event *unusedEvent)
 
 void Widget::pushDownEvent()
 {
+	this->retain();
     //this->retain();  ->3502cb0a23a17967fac4a7bccaf7d2e52297a2c6  有必要就参照这个
     if (_touchEventCallback) {
         _touchEventCallback(this, TouchEventType::BEGAN);
@@ -728,10 +729,12 @@ void Widget::pushDownEvent()
     {
         (_touchEventListener->*_touchEventSelector)(this,TOUCH_EVENT_BEGAN);
     }
+	this->release();
 }
 
 void Widget::moveEvent()
 {
+	this->retain();
     if (_touchEventCallback) {
         _touchEventCallback(this, TouchEventType::MOVED);
     }
@@ -740,11 +743,12 @@ void Widget::moveEvent()
     {
         (_touchEventListener->*_touchEventSelector)(this,TOUCH_EVENT_MOVED);
     }
+	this->release();
 }
 
 void Widget::releaseUpEvent()
 {
-    
+    this->retain();
     if (_touchEventCallback) {
         _touchEventCallback(this, TouchEventType::ENDED);
     }
@@ -753,10 +757,12 @@ void Widget::releaseUpEvent()
     {
         (_touchEventListener->*_touchEventSelector)(this,TOUCH_EVENT_ENDED);
     }
+	this->release();
 }
 
 void Widget::cancelUpEvent()
 {
+	this->retain();
     if (_touchEventCallback)
     {
         _touchEventCallback(this, TouchEventType::CANCELED);
@@ -766,7 +772,7 @@ void Widget::cancelUpEvent()
     {
         (_touchEventListener->*_touchEventSelector)(this,TOUCH_EVENT_CANCELED);
     }
-   
+    this->release();
 }
 
 void Widget::addTouchEventListener(Ref *target, SEL_TouchEvent selector)
