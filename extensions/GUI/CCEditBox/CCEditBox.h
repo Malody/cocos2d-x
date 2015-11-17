5ce719827dead50105d2794cd1aaeddb3ab81e0b
dolly <woc2006@163.com>
2015-11-17 20:30:28 +0800

    修复android 用户名输入问题

--- extensions/GUI/CCEditBox/CCEditBox.h
+++ extensions/GUI/CCEditBox/CCEditBox.h
@@ -1,19 +1,19 @@
 /****************************************************************************
  Copyright (c) 2010-2012 cocos2d-x.org
  Copyright (c) 2012 James Chen
- 
+
  http://www.cocos2d-x.org
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -36,7 +36,7 @@ class EditBox;
 class EditBoxImpl;
 
 
-class EditBoxDelegate 
+class EditBoxDelegate
 {
 public:
     /**
@@ -44,7 +44,7 @@ public:
      * @lua NA
      */
     virtual ~EditBoxDelegate() {};
-    
+
     /**
      * This method is called when an edit box gains focus after keyboard is shown.
      * @param editBox The edit box object that generated the event.
@@ -52,8 +52,8 @@ public:
      * @lua NA
      */
     virtual void editBoxEditingDidBegin(EditBox* editBox) {};
-    
-    
+
+
     /**
      * This method is called when an edit box loses focus after keyboard is hidden.
      * @param editBox The edit box object that generated the event.
@@ -61,7 +61,7 @@ public:
      * @lua NA
      */
     virtual void editBoxEditingDidEnd(EditBox* editBox) {};
-    
+
     /**
      * This method is called when the edit box text was changed.
      * @param editBox The edit box object that generated the event.
@@ -70,7 +70,7 @@ public:
      * @lua NA
      */
     virtual void editBoxTextChanged(EditBox* editBox, const std::string& text) {};
-    
+
     /**
      * This method is called when the return button was pressed or the outside area of keyboard was touched.
      * @param editBox The edit box object that generated the event.
@@ -78,18 +78,18 @@ public:
      * @lua NA
      */
 	virtual void editBoxReturn(EditBox* editBox) {};
-	
+
 	virtual void editBoxControlKey(EditBox* editBox, int key) {};
-    
+
 };
 
 /**
  * \brief Class for edit box.
  *
  * You can use this widget to gather small amounts of text from the user.
- * 
+ *
  */
- 
+
 class EditBox
 : public ControlButton
 , public IMEDelegate
@@ -103,7 +103,7 @@ public:
         SEARCH,
         GO
     };
-    
+
     /**
      * \brief The EditBox::InputMode defines the type of text that the user is allowed
      * to enter.
@@ -114,39 +114,39 @@ public:
          * The user is allowed to enter any text, including line breaks.
          */
         ANY,
-        
+
         /**
          * The user is allowed to enter an e-mail address.
          */
         EMAIL_ADDRESS,
-        
+
         /**
          * The user is allowed to enter an integer value.
          */
         NUMERIC,
-        
+
         /**
          * The user is allowed to enter a phone number.
          */
         PHONE_NUMBER,
-        
+
         /**
          * The user is allowed to enter a URL.
          */
         URL,
-        
+
         /**
          * The user is allowed to enter a real number value.
          * This extends kEditBoxInputModeNumeric by allowing a decimal point.
          */
         DECIMAL,
-        
+
         /**
          * The user is allowed to enter any text, except for line breaks.
          */
         SINGLE_LINE,
     };
-    
+
     /**
      * \brief The EditBox::InputFlag defines how the input text is displayed/formatted.
      */
@@ -157,7 +157,7 @@ public:
          * obscured whenever possible. This implies EDIT_BOX_INPUT_FLAG_SENSITIVE.
          */
         PASSWORD,
-        
+
         /**
          * Indicates that the text entered is sensitive data that the
          * implementation must never store into a dictionary or table for use
@@ -165,25 +165,27 @@ public:
          * A credit card number is an example of sensitive data.
          */
         SENSITIVE,
-        
+
         /**
          * This flag is a hint to the implementation that during text editing,
          * the initial letter of each word should be capitalized.
          */
         INITIAL_CAPS_WORD,
-        
+
         /**
          * This flag is a hint to the implementation that during text editing,
          * the initial letter of each sentence should be capitalized.
          */
         INITIAL_CAPS_SENTENCE,
-        
+
         /**
          * Capitalize all characters automatically.
          */
         INTIAL_CAPS_ALL_CHARACTERS,
+
+        ORIGINAL
     };
-    
+
     /**
      * create a edit box with size.
      * @return An autorelease pointer of EditBox, you don't need to release it only if you retain it again.
@@ -195,7 +197,7 @@ public:
      * @js ctor
      */
     EditBox(void);
-    
+
     /**
      * Destructor.
      * @js NA
@@ -208,7 +210,7 @@ public:
      * @param size The size of edit box.
      */
     bool initWithSizeAndBackgroundSprite(const Size& size, Scale9Sprite* pNormal9SpriteBg);
-    
+
     /**
      * Gets/Sets the delegate for edit box.
      * @lua NA
@@ -219,7 +221,7 @@ public:
      * @lua NA
      */
     EditBoxDelegate* getDelegate();
-    
+
 #if CC_ENABLE_SCRIPT_BINDING
     /**
      * Registers a script function that will be called for EditBox events.
@@ -248,7 +250,7 @@ public:
      * @lua NA
      */
     void registerScriptEditBoxHandler(int handler);
-    
+
     /**
      * Unregisters a script function that will be called for EditBox events.
      * @js NA
@@ -261,91 +263,91 @@ public:
      * @lua NA
      */
     int  getScriptEditBoxHandler(void){ return _scriptEditBoxHandler ;}
-    
+
 #endif // #if CC_ENABLE_SCRIPT_BINDING
-    
+
     /**
      * Set the text entered in the edit box.
      * @param pText The given text.
      */
     void setText(const char* pText);
-    
+
     /**
      * Get the text entered in the edit box.
      * @return The text entered in the edit box.
      */
     const char* getText(void);
-	
+
 	/**
 	 * Set the font.
 	 * @param pFontName The font name.
 	 * @param fontSize The font size.
 	 */
 	void setFont(const char* pFontName, int fontSize);
-    
+
 	/**
 	 * Set the font name.
 	 * @param pFontName The font name.
 	 */
 	void setFontName(const char* pFontName);
-    
+
     /**
 	 * Set the font size.
 	 * @param fontSize The font size.
 	 */
 	void setFontSize(int fontSize);
-    
+
     /**
      * Set the font color of the widget's text.
      */
     void setFontColor(const Color3B& color);
-    
+
 	/**
 	 * Set the placeholder's font.
 	 * @param pFontName The font name.
 	 * @param fontSize The font size.
 	 */
 	void setPlaceholderFont(const char* pFontName, int fontSize);
-    
+
     /**
 	 * Set the placeholder's font name.
 	 * @param pFontName The font name.
 	 */
 	void setPlaceholderFontName(const char* pFontName);
-    
+
     /**
 	 * Set the placeholder's font size.
 	 * @param fontSize The font size.
 	 */
 	void setPlaceholderFontSize(int fontSize);
-    
+
     /**
      * Set the font color of the placeholder text when the edit box is empty.
      * Not supported on IOS.
      */
     void setPlaceholderFontColor(const Color3B& color);
-	
+
 	void setHighlightColor(const Color3B& color) { _highlight = color; };
-    
+
     /**
      * Set a text in the edit box that acts as a placeholder when an
      * edit box is empty.
      * @param pText The given text.
      */
     void setPlaceHolder(const char* pText);
-    
+
     /**
      * Get a text in the edit box that acts as a placeholder when an
      * edit box is empty.
      */
     const char* getPlaceHolder(void);
-    
+
     /**
      * Set the input mode of the edit box.
      * @param inputMode One of the EditBox::InputMode constants.
      */
     void setInputMode(InputMode inputMode);
-    
+
     /**
      * Sets the maximum input length of the edit box.
      * Setting this value enables multiline input mode by default.
@@ -354,26 +356,26 @@ public:
      * @param maxLength The maximum length.
      */
     void setMaxLength(int maxLength);
-    
+
     /**
      * Gets the maximum input length of the edit box.
      *
      * @return Maximum input length.
      */
     int getMaxLength();
-    
+
     /**
      * Set the input flags that are to be applied to the edit box.
      * @param inputFlag One of the EditBox::InputFlag constants.
      */
     void setInputFlag(InputFlag inputFlag);
-    
+
     /**
      * Set the return type that are to be applied to the edit box.
      * @param returnType One of the EditBox::KeyboardReturnType constants.
      */
     void setReturnType(EditBox::KeyboardReturnType returnType);
-    
+
     /* override functions */
     virtual void setPosition(const Vec2& pos) override;
     virtual void setVisible(bool visible) override;
@@ -414,36 +416,36 @@ public:
      * @lua NA
      */
     virtual void keyboardDidHide(IMEKeyboardNotificationInfo& info) override;
-    
+
     /* callback funtions
      * @js NA
      * @lua NA
      */
     virtual void touchDownAction(Ref *sender, Control::EventType controlEvent);
-	
+
 	void closeKeyboard();
 protected:
     void updatePosition(float dt);
     EditBoxImpl*      _editBoxImpl;
     EditBoxDelegate*  _delegate;
-    
+
     InputMode    _editBoxInputMode;
     InputFlag    _editBoxInputFlag;
     EditBox::KeyboardReturnType  _keyboardReturnType;
-    
+
     std::string _text;
     std::string _placeHolder;
-    
+
     std::string _fontName;
     std::string _placeholderFontName;
-    
+
     int _fontSize;
     int _placeholderFontSize;
-    
+
     Color3B _colText;
     Color3B _colPlaceHolder;
 	Color3B _highlight;
-    
+
     int   _maxLength;
     float _adjustHeight;
 #if CC_ENABLE_SCRIPT_BINDING
@@ -454,4 +456,3 @@ protected:
 NS_CC_EXT_END
 
 #endif /* __CCEDITTEXT_H__ */
-
--- extensions/GUI/CCEditBox/CCEditBoxImplAndroid.cpp
+++ extensions/GUI/CCEditBox/CCEditBoxImplAndroid.cpp
@@ -1,19 +1,19 @@
 /****************************************************************************
  Copyright (c) 2010-2012 cocos2d-x.org
  Copyright (c) 2012 James Chen
- 
+
  http://www.cocos2d-x.org
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -44,23 +44,23 @@ EditBoxImplAndroid::EditBoxImplAndroid(EditBox* pEditText)
 , _label(nullptr)
 , _labelPlaceHolder(nullptr)
 , _editBoxInputMode(EditBox::InputMode::SINGLE_LINE)
-, _editBoxInputFlag(EditBox::InputFlag::INTIAL_CAPS_ALL_CHARACTERS)
+, _editBoxInputFlag(EditBox::InputFlag::ORIGINAL)
 , _keyboardReturnType(EditBox::KeyboardReturnType::DEFAULT)
 , _colText(Color3B::WHITE)
 , _colPlaceHolder(Color3B::GRAY)
 , _maxLength(-1)
 {
-    
+
 }
 
 EditBoxImplAndroid::~EditBoxImplAndroid()
 {
-	
+
 }
 
 void EditBoxImplAndroid::doAnimationWhenKeyboardMove(float duration, float distance)
 { // don't need to be implemented on android platform.
-	
+
 }
 
 static const int CC_EDIT_BOX_PADDING = 5;
@@ -75,7 +75,7 @@ bool EditBoxImplAndroid::initWithSize(const Size& size)
     _label->setPosition(Vec2(CC_EDIT_BOX_PADDING, size.height / 2.0f));
     _label->setColor(_colText);
     _editBox->addChild(_label);
-	
+
     _labelPlaceHolder = Label::create();
     _labelPlaceHolder->setSystemFontSize(size.height-12);
 	// align the text vertically center
@@ -84,7 +84,7 @@ bool EditBoxImplAndroid::initWithSize(const Size& size)
     _labelPlaceHolder->setVisible(false);
     _labelPlaceHolder->setColor(_colPlaceHolder);
     _editBox->addChild(_labelPlaceHolder);
-    
+
     _editSize = size;
     return true;
 }
@@ -95,7 +95,7 @@ void EditBoxImplAndroid::setFont(const char* pFontName, int fontSize)
 		_label->setSystemFontName(pFontName);
 		_label->setSystemFontSize(fontSize);
 	}
-	
+
 	if(_labelPlaceHolder != NULL) {
 		_labelPlaceHolder->setSystemFontName(pFontName);
 		_labelPlaceHolder->setSystemFontSize(fontSize);
@@ -157,13 +157,13 @@ void EditBoxImplAndroid::setText(const char* pText)
     if (pText != NULL)
     {
         _text = pText;
-		
+
         if (_text.length() > 0)
         {
             _labelPlaceHolder->setVisible(false);
-			
+
             std::string strToShow;
-			
+
             if (EditBox::InputFlag::PASSWORD == _editBoxInputFlag)
             {
                 long length = cc_utf8_strlen(_text.c_str(), -1);
@@ -192,7 +192,7 @@ void EditBoxImplAndroid::setText(const char* pText)
             _labelPlaceHolder->setVisible(true);
             _label->setString("");
         }
-		
+
     }
 }
 
@@ -210,14 +210,14 @@ void EditBoxImplAndroid::setPlaceHolder(const char* pText)
         {
             _labelPlaceHolder->setVisible(true);
         }
-		
+
         _labelPlaceHolder->setString(_placeHolder.c_str());
     }
 }
 
 void EditBoxImplAndroid::setPosition(const Vec2& pos)
 { // don't need to be implemented on android platform.
-	
+
 }
 
 void EditBoxImplAndroid::setVisible(bool visible)
@@ -227,29 +227,29 @@ void EditBoxImplAndroid::setVisible(bool visible)
 
 void EditBoxImplAndroid::setContentSize(const Size& size)
 { // don't need to be implemented on android platform.
-	
+
 }
 
 void EditBoxImplAndroid::setAnchorPoint(const Vec2& anchorPoint)
 { // don't need to be implemented on android platform.
-	
+
 }
 
 void EditBoxImplAndroid::visit(void)
 { // don't need to be implemented on android platform.
-    
+
 }
 
 void EditBoxImplAndroid::onEnter(void)
 { // don't need to be implemented on android platform.
-    
+
 }
 
 static void editBoxCallbackFunc(const char* pText, void* ctx)
 {
     EditBoxImplAndroid* thiz = (EditBoxImplAndroid*)ctx;
     thiz->setText(pText);
-	
+
     if (thiz->getDelegate() != NULL)
     {
         thiz->getDelegate()->editBoxTextChanged(thiz->getEditBox(), thiz->getText());
@@ -257,11 +257,11 @@ static void editBoxCallbackFunc(const char* pText, void* ctx)
         thiz->getDelegate()->editBoxReturn(thiz->getEditBox());
         thiz->getDelegate()->editBoxControlKey(thiz->getEditBox(), (int)cocos2d::EventKeyboard::KeyCode::KEY_KP_ENTER);
     }
-    
+
 #if CC_ENABLE_SCRIPT_BINDING
     EditBox* pEditBox = thiz->getEditBox();
     if (NULL != pEditBox && 0 != pEditBox->getScriptEditBoxHandler())
-    {        
+    {
         CommonScriptData data(pEditBox->getScriptEditBoxHandler(), "changed",pEditBox);
         ScriptEvent event(kCommonEvent,(void*)&data);
         ScriptEngineManager::getInstance()->getScriptEngine()->sendEvent(&event);
@@ -283,17 +283,17 @@ void EditBoxImplAndroid::openKeyboard()
     {
         _delegate->editBoxEditingDidBegin(_editBox);
     }
-    
+
 #if CC_ENABLE_SCRIPT_BINDING
     EditBox* pEditBox = this->getEditBox();
     if (NULL != pEditBox && 0 != pEditBox->getScriptEditBoxHandler())
-    {        
+    {
         CommonScriptData data(pEditBox->getScriptEditBoxHandler(), "began",pEditBox);
         ScriptEvent event(cocos2d::kCommonEvent,(void*)&data);
         ScriptEngineManager::getInstance()->getScriptEngine()->sendEvent(&event);
     }
 #endif
-	
+
     showEditTextDialogJNI(  _placeHolder.c_str(),
 						  _text.c_str(),
 						  (int)_editBoxInputMode,
@@ -302,15 +302,14 @@ void EditBoxImplAndroid::openKeyboard()
 						  _maxLength,
 						  editBoxCallbackFunc,
 						  (void*)this  );
-	
+
 }
 
 void EditBoxImplAndroid::closeKeyboard()
 {
-	
+
 }
 
 NS_CC_EXT_END
 
 #endif /* #if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID) */
-
